d1636298917977164f273efd6cae9baa5b56c7b4
Medhi PLUMASSEAU <medhi.plumasseau@gmail.com>
2017-11-28 14:07:47 +0100

    PAR - Medhi PLUMASSEAU

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ var opponent;
 function startGame () {
   ctx = document.getElementById('canvas').getContext('2d');
   player = new Tank(30,30,40,'#475534');
-  opponent = new Bomb(450,100,20, 0, Math.PI*2, true, "red", "black");
-  setInterval(drawEverything,1000);
+  opponent = new Bomb(450,100,20, 0, Math.PI*2, true, 'red', 'black');
+  setInterval(updateEverything,1);
 
   document.onkeydown = function(e) {
     switch (e.keyCode) {
@@ -32,9 +32,19 @@ function startGame () {
         player.moveDown();
         drawEverything();
         break;
+      case 32:
+        player.shoot();
+        drawEverything();
     }
   };
 
+}
+
+function updateEverything () {
+  player.update();
+  drawEverything();
+  /*opponent.update();
+  drawEverything();*/
 }
 
 function drawEverything () {
@@ -44,11 +54,12 @@ function drawEverything () {
 }
 
 
-function Tank (x,y,width,color,vx,vy) {
+function Tank (x,y,width,color) {
   this.x = x;
   this.y = y;
   this.width = width;
   this.color = color;
+  this.bullets = [];
 }
 
 Tank.prototype.moveLeft = function () {
@@ -67,13 +78,17 @@ Tank.prototype.moveDown = function () {
   if (this.y < 310) {this.y += 10;};
 };
 
-Tank.prototype.draw = function () {
-  this.gun = {
+Tank.prototype.getGun = function () {
+  return  {
     xG1: this.x + this.width/2,
     yG1: this.y + this.width/2,
     xG2: this.x + 3*this.width/2,
     yG2: this.y + this.width/2
   };
+};
+
+Tank.prototype.draw = function () {
+  this.gun = this.getGun();
 
   ctx.fillStyle = this.color;
   ctx.fillRect(this.x, this.y, this.width, this.width);
@@ -84,21 +99,46 @@ Tank.prototype.draw = function () {
   ctx.moveTo(this.gun.xG1, this.gun.yG1);
   ctx.lineTo(this.gun.xG2, this.gun.yG2);
   ctx.stroke();
+
+  for (var i = 0; i < this.bullets.length; i++) {
+    this.bullets[i].draw();
+  }
 };
 
-function Shootbullet () {
-  $(window).keypress(function(e) {
-    if (e.which === 32) {
-	     console.log('Hi');
-    }
-  });
+Tank.prototype.shoot = function () {
+  var bullet = new Bullet(this.getGun().xG2,this.getGun().yG2-5/2, 1, 0,  5,"#e57e09");
+  this.bullets.push(bullet);
+};
+
+Tank.prototype.update = function () {
+    for (var i = 0; i < this.bullets.length; i++) {
+    this.bullets[i].update();
+  }
+};
+
+function Bullet (x,y,vx,vy,width,color) {
+  this.x = x;
+  this.y = y;
+  this.vx = vx;
+  this.vy = vy;
+  this.width = width;
+  this.color = color;
 }
 
-function DrawBullet () {
+Bullet.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.fillRect(this.x, this.y, this.width, this.width);
+  ctx.moveTo(this.x, this.y);
+};
 
-}
+Bullet.prototype.update = function () {
+  this.x += this.vx;
+  this.y += this.vy;
+};
 
-
+//vx --> vers le Tank
+//vy --> vers le Tank
 function Bomb (x, y, radius, startAngle, endAngle, anticlockwise, color, stroke) {
   ctx.beginPath();
   this.x = x;
@@ -109,7 +149,6 @@ function Bomb (x, y, radius, startAngle, endAngle, anticlockwise, color, stroke)
   this.anticlockwise = true;
   this.color = color;
   this.stroke = stroke;
-
 }
 
 Bomb.prototype.draw = function () {
@@ -120,3 +159,8 @@ Bomb.prototype.draw = function () {
   ctx.stroke();
   ctx.fill();
 };
+
+Bomb.prototype.update = function () {
+  this.x += player.x;
+  this.y += player.y;
+}
